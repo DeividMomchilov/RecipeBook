5f4d523acb8df27c661742c92ef8e4f1f9965b64
Deivid Momchilov <deividmom2006@gmail.com>
2026-03-18 18:53:47 +0200

    Add route for the RecipeDetails page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router";
 import Home from "./components/home/Home";
+import RecipeDetails from "./components/recipe-details/RecipeDetails";
 
 
 function App() {
   return (
     <Routes>
       <Route index element={<Home />} />
+      <Route path="/recipe/:id" element={<RecipeDetails/>}/>
     </Routes>
   );
 }
